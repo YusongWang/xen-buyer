d10d91aa430eaaa7fc32da7a15bfb6b4f264b67a
yusong wang <admin@wangyusong.com>
2022-10-14 08:39:46 +0800

    update

--- contracts/buyer.sol
+++ contracts/buyer.sol
@@ -9,29 +9,44 @@ interface XEN {
     function claimMintRewardAndShare(address other, uint256 pct) external;    
 }
 
+interface IBuyer{
+    function fetchAddr() external view returns(XEN);
+}
+
 contract MintForMint is Ownable{
     address public _owner;
     uint8 public _day;
+    IBuyer public _buyer;    
     XEN public _token;
 
-    constructor(XEN token,address payable owner, uint8 day) {
-        token.claimRank(day);        
-        _token = token;
+    constructor(IBuyer buyer,address payable owner, uint8 day) {        
+        _buyer = buyer;
         _owner = owner;
         _day = day;
     }
 
+    function buy() public onlyOwner{
+        _token = _buyer.fetchAddr();
+        _token.claimRank(_day);
+    }
+
     function claim() external onlyOwner {
+        _token = _buyer.fetchAddr();
         _token.claimMintRewardAndShare(_owner,100);
         selfdestruct(payable(_owner));
     }
 }
 
-contract Buyer is Ownable{
+contract Buyer is Ownable,IBuyer{
     using Counters for Counters.Counter;
+
+    Counters.Counter public fatoryTracker;
     Counters.Counter public idTracker;
     Counters.Counter public claimTracker;
 
+
+    uint256 public constant SECONDS_IN_DAY = 3_600 * 24;
+    
     uint8 public _day;
     address payable public _owner;
     XEN public _addr;
@@ -41,7 +56,7 @@ contract Buyer is Ownable{
         MintForMint mint;
     }
 
-    mapping(uint256 => Info) private mints;
+    mapping(uint256 => Info) public mints;
 
     constructor(
         address payable owner,
@@ -53,15 +68,36 @@ contract Buyer is Ownable{
         _addr = addr;
     }
 
+
+    function fetchAddr() public view returns(XEN) {
+        return _addr;
+    }
+
+    function setAddr(XEN addr) external onlyOwner {
+        _addr = addr;
+    }
+
+    function factory(uint8 number) external onlyOwner {
+        for (uint i = 0; i < number; i++) {
+            //idTracker.
+            uint256 id = fatoryTracker.current();            
+            MintForMint mint = new MintForMint(IBuyer(address(this)),_owner, _day);
+            Info memory infos;
+            infos.claimday = 0;
+            infos.mint = mint;
+            mints[id] = infos;
+            fatoryTracker.increment();   
+        }
+    }
+
     function buyToken(uint8 number) external onlyOwner {
         for (uint i = 0; i < number; i++) {
             //idTracker.
             uint256 id = idTracker.current();            
-            MintForMint mint = new MintForMint(_addr,_owner, _day);
-            Info memory infos;
-            infos.claimday = block.timestamp;
-            infos.mint = mint;
-            mints[id] = infos;
+            Info memory info = mints[id];
+            info.claimday = block.timestamp + _day * SECONDS_IN_DAY;
+            info.mint.buy();
+            mints[id] = info;
             idTracker.increment();   
         }
     }
@@ -70,7 +106,7 @@ contract Buyer is Ownable{
         for (uint i = 0; i < number; i++) {
             //idTracker.
             uint256 id = claimTracker.current();            
-            Info memory info = mints[id];
+            Info memory info = mints[id];     
             info.mint.claim();
             claimTracker.increment();   
         }
--- test/Buyer.js
+++ test/Buyer.js
@@ -5,6 +5,7 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { factory } = require("typescript");
 
 function eth(value) {
   return utils.parseEther(value);
@@ -20,8 +21,7 @@ describe("Buyer", function () {
     const Math = await ethers.getContractFactory("Math");
     const math = await Math.deploy();
 
-
-    const XENCryptoFac = await ethers.getContractFactory("XENCrypto",{
+    const XENCryptoFac = await ethers.getContractFactory("XENCrypto", {
       libraries: {
         Math: math.address,
       },
@@ -29,16 +29,17 @@ describe("Buyer", function () {
     xen = await XENCryptoFac.deploy();
 
     const Buyer = await ethers.getContractFactory("Buyer");
-    buyer = await Buyer.deploy(sender.address,"1",xen.address);
+    buyer = await Buyer.deploy(sender.address, "1", xen.address);
     console.log(buyer.address);
   });
 
   it("buy success", async function () {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-    let [sender,b] = await ethers.getSigners();
+    let [sender, b] = await ethers.getSigners();
     //await buyer.connect(b).buyToken("10");
-    
+    await buyer.factory("10");
+    console.log(await buyer.fatoryTracker());
     await buyer.buyToken("10");
     console.log(await buyer.idTracker());
     await time.increaseTo(unlockTime);
@@ -47,8 +48,6 @@ describe("Buyer", function () {
     console.log(await buyer.idTracker());
     let balance = await xen.balanceOf(sender.address);
     console.log(balance);
-    expect(balance.toString()).to.equal("749000000000000000000");    
+    expect(balance.toString()).to.equal("749000000000000000000");
   });
-
-
 });
